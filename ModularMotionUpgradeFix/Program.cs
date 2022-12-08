2744e1f4975b877b66898ae46079d3b373704d20
Hina <919835909@qq.com>
2022-12-08 15:23:38 +0800

    Fix

---
+++ ModularMotionUpgradeFix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ModularMotionUpgradeFix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Assets Folder Path: ");
+            var path = Console.ReadLine();
+            
+            Console.Write("Old GUID: ");
+            var oldGuid = Console.ReadLine();
+            
+            Console.Write("New GUID: ");
+            var newGuid = Console.ReadLine();
+            
+            Fix(path, oldGuid, newGuid);
+        }
+
+        private static void Fix(string path, string g1, string g2)
+        {
+            foreach (var fileInfo in Directory.GetFiles(path,"*.*", SearchOption.AllDirectories))
+            {
+                if (fileInfo.EndsWith(".unity") || fileInfo.EndsWith(".prefab"))
+                {
+                    Console.Out.Write($"{fileInfo.Split("/").Last()}");
+                    var file = File.ReadAllText(fileInfo);
+                    if (!file.Contains(g1))
+                    {
+                        Console.Out.WriteLine($"    skip");
+                        continue;
+                    }
+
+                    Console.Out.WriteLine($"    replaced");
+                    file = file.Replace(g1, g2);
+                    File.WriteAllText(fileInfo, file);
+                }
+            }
+        }
+    }
+}
